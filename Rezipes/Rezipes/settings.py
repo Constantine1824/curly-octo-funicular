4ff22dfe99bf80ad3480879d0815680f5c4ab5fb
Ayomide <ayomidet905@gmail.com>
2023-11-01 03:03:07 +0100

    Disabled whitenoise storages temporarily

--- Rezipes/Rezipes/settings.py
+++ Rezipes/Rezipes/settings.py
@@ -142,11 +142,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STORAGES = {
-    "staticfiles" : {
-        "BACKEND" : "whitenoise.storage.CompressedManifestStaticFilesStorage"
-    }
-}
+# STORAGES = {
+#     "staticfiles" : {
+#         "BACKEND" : "whitenoise.storage.CompressedManifestStaticFilesStorage"
+#     }
+# }
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
