db297cc2c88a9b4252157352befeabc126c52830
Ayomide <ayomidet905@gmail.com>
2023-11-01 02:29:54 +0100

    Changed database to postgres

--- Rezipes/Rezipes/settings.py
+++ Rezipes/Rezipes/settings.py
@@ -89,8 +89,12 @@ WSGI_APPLICATION = 'Rezipes.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'HOST' : os.environ.get('DB_HOST'),
+        'USER' : os.environ.get('DB_USER'),
+        'PASSWORD' : os.environ.get('DB_PASSWORD'),
+        'PORT' : os.environ.get('DB_PORT')
     }
 }
 
